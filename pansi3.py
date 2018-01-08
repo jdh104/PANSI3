cbea4fb567d0a09f94376d46ae502e79532e8a0c
Jonah Haney <jdh104@users.noreply.github.com>
2018-01-08 16:10:53 -0600

    Update pansi
    Add functionality for
    * setting fonts
    * debugging
    * Chained method calls
         * ex: pansi_instance.saveCursorPosition().moveCursorUp(5).setBold().setForeground(3,3,200).clearToBottom()
    Change `pansi.output()` so that cursor position is independent from output (from user perspective)

--- pansi3.py
+++ pansi3.py
@@ -2,7 +2,9 @@
 
 PRGM_AUTHOR = "Jonah Haney"
 PRGM_VERSION = "2018.1.7"
-DEBUG = False
+DEBUG = False#True 
+
+import sys
 
 ESC = "\033"
 STX = "\02"
@@ -46,169 +48,180 @@ BCLR = "48"     # Set background color
 
 class pansi:
 
-	def printANSI(self, C1, *args, ender="", seper=";"):
+	def printf(self, *args, sep=" ", end="\n", file=sys.stdout, flush=False):
+		return self
+
+	def printANSI(self, C1, *args, ender="", seper=";", file=sys.stdout):
 		"""Construct and Print an ANSI Escape Code"""
-		print(ESC, C1, sep="", end="")
-		if args:
-			print(*args, end="", sep=seper, flush=True)
-		print(ender, end="")
 		if DEBUG:
-			print("\n", C1, *args, end=ender, sep=seper, file=sys.stderr)
+			print("\n", C1, end="", sep="", file=sys.stderr)
+			print(*args, end=ender, sep=seper, file=sys.stderr)
+		else:
+			print(ESC, C1, sep="", end="")
+			if args:
+				print(*args, end="", sep=seper, flush=True, file=file)
+			print(end=ender)
+		return self
 
-	def printCS(self, code, *args):
+	def printCS(self, code, *args, file=sys.stdout):
 		"""Construct and Print a Control Sequence"""
-		self.printANSI(CSI, *args, ender=code)
+		return self.printANSI(CSI, *args, ender=code, file=file)
 
-	def printSGR(self, *args):
+	def printSGR(self, *args, file=sys.stdout):
 		"""Construct and Print a Select Graphic Rendition Sequence"""
-		self.printCS(SGR, *args)
+		return self.printCS(SGR, *args, file=file)
 
-	def printOSC(self, *args):
+	def printOSC(self, *args, file=sys.stdout):
 		"""Construct and Print an Operating System Command"""
-		self.printANSI(OSC, *args)
+		return self.printANSI(OSC, *args, file=file)
 
-	def printRIS(self):
+	def printRIS(self, file=sys.stdout):
 		"""Reset the screen to its original state"""
-		self.printANSI(RIS)
+		return self.printANSI(RIS, file=file)
 
-	def resetAll(self):
+	def reset(self, file=sys.stdout):
 		"""Reset the screen to its original state"""
-		self.printRIS()
+		return self.printRIS(file=file)
 
-	def resetStyle(self):
+	def resetStyle(self, file=sys.stdout):
 		"""Turn off all SGR attributes"""
-		self.printSGR(RSET)
+		return self.printSGR(RSET, file=file)
 
-	def clearToTop(self):
+	def clearToTop(self, file=sys.stdout):
 		"""Clear the screen from cursor to top"""
-		self.printCS(ED, 0)
+		return self.printCS(ED, 1, file=file)
 
-	def clearToBottom(self):
+	def clearToBottom(self, file=sys.stdout):
 		"""Clear the screen from cursor to bottom"""
-		self.printCS(ED, 1)
+		return self.printCS(ED, 0, file=file)
 
-	def clear(self):
+	def clear(self, file=sys.stdout):
 		"""Clear the screen from top to bottom"""
-		self.printCS(ED, 2)
+		return self.printCS(ED, 2, file=file)
 
-	def scrollUp(self, scroll = 1):
+	def scrollUp(self, scroll = 1, file=sys.stdout):
 		"""Scroll the screen up 'scroll' lines"""
 		if scroll > 0:
-			self.printCS(SU, scroll)
+			return self.printCS(SU, scroll, file=file)
+		return self
 
-	def scrollDown(self, scroll = 1):
+	def scrollDown(self, scroll = 1, file=sys.stdout):
 		"""Scroll the screen down 'scroll' lines"""
 		if scroll > 0:
-			self.printCS(SD, scroll)
+			return self.printCS(SD, scroll, file=file)
+		return self
 
-	def scroll(self, signed_scroll):
+	def scroll(self, signed_scroll, file=sys.stdout):
 		"""Scrolls down if 'signed_scroll' is positive, up if negative"""
 		if signed_scroll < 0:
-			self.scrollUp(-signed_scroll)
+			return self.scrollUp(-signed_scroll, file=file)
 		elif signed_scroll > 0:
-			self.scrollDown(signed_scroll)
+			return self.scrollDown(signed_scroll, file=file)
+		return self
 
-	def moveCursorTo(self, new_cursor_x, new_cursor_y):
+	def moveCursorTo(self, new_cursor_x, new_cursor_y, file=sys.stdout):
 		"""Move the cursor to the specified position"""
 		if new_cursor_x > 0 and new_cursor_y > 0:
-			self.printCS(CUP, new_cursor_x, new_cursor_y)
+			return self.printCS(CUP, new_cursor_x, new_cursor_y, file=file)
+		return self
 
-	def moveCursorUp(self, n = 1):
+	def moveCursorUp(self, n = 1, file=sys.stdout):
 		"""Move the cursor up 'n' cells"""
 		if n > 0:
-			self.printCS(CUU, n)
+			return nself.printCS(CUU, n, file=file)
+		return self
 
-	def moveCursorDown(self, n = 1):
+	def moveCursorDown(self, n = 1, file=sys.stdout):
 		"""Move the cursor down 'n' cells"""
 		if n > 0:
-			self.printCS(CUD, n)
+			return self.printCS(CUD, n, file=file)
+		return self
 
-	def moveCursorLeft(self, n = 1):
+	def moveCursorLeft(self, n = 1, file=sys.stdout):
 		"""Move the cursor left (back) 'n' cells"""
 		if n > 0:
-			self.printCS(CUB, n)
+			return self.printCS(CUB, n, file=file)
+		return self
 
-	def moveCursorRight(self, n = 1):
+	def moveCursorRight(self, n = 1, file=sys.stdout):
 		"""Move the cursor right (forward) 'n' cells"""
 		if n > 0:
-			self.printCS(CUF, n)
+			return self.printCS(CUF, n, file=file)
+		return self
 
-	def newLine(self, n = 1):
+	def newLine(self, n = 1, file=sys.stdout):
 		"""Move the cursor to beginning of 'n' lines down"""
-		self.printCS(CNL, n)
+		return self.printCS(CNL, n, file=file)
 
-	def prevLine(self, n = 1):
+	def prevLine(self, n = 1, file=sys.stdout):
 		"""Move the cursor to beginning of 'n' lines up"""
-		self.printCS(CPL, n)
+		return self.printCS(CPL, n, file=file)
 
-	def setForeground(self, *args):
+	def setForeground(self, *args, file=sys.stdout):
 		"""Set the text color. Use color code or R, G, B as arguments"""
 		if len(args) == 3:
-			self.printSGR(FCLR, 2, *args)
+			return self.printSGR(FCLR, 2, *args, file=file)
 		elif len(args) == 1:
-			self.printSGR(FCLR, 5, args[0])
+			return self.printSGR(FCLR, 5, args[0], file=file)
+		return self
 
-	def setBackground(self, *args):
+	def setBackground(self, *args, file=sys.stdout):
 		"""Set the background color. Use color code or R, G, B as arguments"""
 		if len(args) == 3:
-			self.printSGR(BCLR, 2, *args)
+			return self.printSGR(BCLR, 2, *args, file=file)
 		elif len(args) == 1:
-			self.printSGR(BCLR, 5, arg[0])
+			return self.printSGR(BCLR, 5, arg[0], file=file)
+		return self
 
-	def invertColors(self):
+	def invertColors(self, file=sys.stdout):
 		"""Set foreground color to background color, and vice-versa"""
-		self.printSGR(RVRS)
+		return self.printSGR(RVRS, file=file)
 
-	def setBold(self, *args):
+	def setBold(self, *args, file=sys.stdout):
 		"""Set boldness: setBold() == setBold(True)"""
 		if len(args) == 1:
 			if args[0]:
-				self.printSGR(BOLD)
-			else:
-				self.printSGR(BOLD_OFF)
-		else:
-			self.printSGR(BOLD)
+				return self.printSGR(BOLD, file=file)
+			return self.printSGR(BOLD_OFF, file=file)
+		return self.printSGR(BOLD, file=file)
 
-	def setUnderline(self, *args):
+	def setUnderline(self, *args, file=sys.stdout):
 		"""Set Underline: setUnderline() == setUnderline(True)"""
 		if len(args) == 1:
 			if args[0]:
-				self.printSGR(UNDL)
-			else:
-				self.printSGR(UNDL_OFF)
-		else:
-			self.printSGR(UNDL)
+				return self.printSGR(UNDL, file=file)
+			return self.printSGR(UNDL_OFF, file=file)
+		return self.printSGR(UNDL, file=file)
 
-	def setBlink(self, *args):
+	def setBlink(self, *args, file=sys.stdout):
 		"""Set Slow Blink: setBlink() == setBlink(True)"""
 		if len(args) == 1:
 			if args[0]:
-				self.printSGR(BLNK)
-			else:
-				self.printSGR(BLNK_OFF)
-		else:
-			self.printSGR(BLNK)
-			
-	def setFont(self, font_code):
+				return self.printSGR(BLNK, file=file)
+			return self.printSGR(BLNK_OFF, file=file)
+		return self.printSGR(BLNK, file=file)
+
+	def setFont(self, font_code, file=sys.stdout):
 		"""Use alternative font, or reset to default"""
-		if font_code > 9 and font_code < 20:
-			self.printSGR(FONT[font_code])
-		else:
-			self.printSGR(DFNT)
+		if font_code > 0 and font_code < 10:
+			return self.printSGR(FONT[font_code], file=file)
+		return self.printSGR(DFNT, file=file)
 	
-	def resetFont(self):
+	def resetFont(self, file=sys.stdout):
 		"""Reset to default font"""
-		self.printSGR(DFNT)
+		return self.printSGR(DFNT, file=file)
 
-	def output(self, x, y, *args):
-		"""Print *args at location x,y"""
-		self.moveCursorTo(x, y)
-		print(*args)
-
-	def saveCursorPosition(self):
+	def saveCursorPosition(self, file=sys.stdout):
 		"""Save current cursor position"""
-		self.printCS(SCP)
+		return self.printCS(SCP, file=file)
 
-	def restoreCursorPosition(self):
+	def restoreCursorPosition(self, file=sys.stdout):
 		"""Restore saved cursor position"""
-		self.printCS(RCP)
+		return self.printCS(RCP, file=file)
+
+	def output(self, x, y, *args, end="", sep="", file=sys.stdout):
+		"""Print *args at location x,y"""
+		# self.printf("{SCP}{CUP,",x,",",y,"}{RCP}",*args, sep=sep, end=end, file=file)
+		self.saveCursorPosition().moveCursorTo(x, y, file=file)
+		print(*args, end=end, sep=sep, file=file)
+		return self.restoreCursorPosition()
